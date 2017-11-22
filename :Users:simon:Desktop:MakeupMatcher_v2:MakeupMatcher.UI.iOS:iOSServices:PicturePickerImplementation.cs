24338d434aea85729b8dbdb1cfca8de0c79cdc00
Simon Vanleeuw <simon-vanleeuw@hotmail.com>
2017-11-22 22:04:38 +0100

    details verfijnen deel 2

---
+++ :Users:simon:Desktop:MakeupMatcher_v2:MakeupMatcher.UI.iOS:iOSServices:PicturePickerImplementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using MakeupMatcher.Core.Services;
+using UIKit;
+
+namespace MakeupMatcher.UI.iOS.iOSServices
+{
+    public class PicturePickerImplementation : IPicturePicker
+    {
+        TaskCompletionSource<Stream> taskCompletionSource;
+        UIImagePickerController imagePicker;
+
+        public Task<Stream> GetImageStreamAsync()
+        {
+            imagePicker = new UIImagePickerController
+            {
+                SourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+                MediaTypes = UIImagePickerController.AvailableMediaTypes(UIImagePickerControllerSourceType.PhotoLibrary);
+            }:
+
+            imagePicker.FinishedPickingMedia += OnImagePickerFinishedPickingMedia;
+            imagePicker.Canceled += OnImagePickerCancelled;
+
+            UIWindow window = UIApplication.SharedApplication.KeyWindow;
+            var viewController = Root
+        }
+    }
+}
